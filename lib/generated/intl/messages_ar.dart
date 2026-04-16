b519e6ad78da3f856884b9c0327113851e406c43
sh2 <tmoawad26@gamil.com>
2026-04-17 01:39:54 +0200

    Create Localization to dashboard_responsive app

---
+++ lib/generated/intl/messages_ar.dart
@@ -0,0 +1,36 @@
+// DO NOT EDIT. This is code generated via package:intl/generate_localized.dart
+// This is a library that provides messages for a ar locale. All the
+// messages from the main program should be duplicated here with the same
+// function name.
+
+// Ignore issues from commonly used lints in this file.
+// ignore_for_file:unnecessary_brace_in_string_interps, unnecessary_new
+// ignore_for_file:prefer_single_quotes,comment_references, directives_ordering
+// ignore_for_file:annotate_overrides,prefer_generic_function_type_aliases
+// ignore_for_file:unused_import, file_names, avoid_escaping_inner_quotes
+// ignore_for_file:unnecessary_string_interpolations, unnecessary_string_escapes
+
+import 'package:intl/intl.dart';
+import 'package:intl/message_lookup_by_library.dart';
+
+final messages = new MessageLookup();
+
+typedef String MessageIfAbsent(String messageStr, List<dynamic> args);
+
+class MessageLookup extends MessageLookupByLibrary {
+  String get localeName => 'ar';
+
+  final messages = _notInlinedMessages(_notInlinedMessages);
+  static Map<String, Function> _notInlinedMessages(_) => <String, Function>{
+    "Welcome": MessageLookupByLibrary.simpleMessage("اهلا بالعالم"),
+    "email": MessageLookupByLibrary.simpleMessage("Madraniadi20@gmail.com"),
+    "investments": MessageLookupByLibrary.simpleMessage("استثماراتى"),
+    "layout_account": MessageLookupByLibrary.simpleMessage("تسجيل الخروج"),
+    "name": MessageLookupByLibrary.simpleMessage("ليكان أوكيوو"),
+    "periodType": MessageLookupByLibrary.simpleMessage("شهريا"),
+    "setting_system": MessageLookupByLibrary.simpleMessage("نظام الاعدادات"),
+    "title": MessageLookupByLibrary.simpleMessage("انشاء لوحة تحكم متجاوبة"),
+    "transactions": MessageLookupByLibrary.simpleMessage("عملياتى"),
+    "wallet_account": MessageLookupByLibrary.simpleMessage("حساب المحفظة"),
+  };
+}
