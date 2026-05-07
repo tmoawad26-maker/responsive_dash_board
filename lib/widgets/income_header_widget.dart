a3e4d260151009c8da140b856ca4c6e20cfaaf81
sh2 <tmoawad26@gamil.com>
2026-05-07 18:53:59 +0300

    custom income header widget

---
+++ lib/widgets/income_header_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/core/utilts/constants.dart';
+import 'package:responsive_dash_board/core/utilts/styles.dart';
+import 'package:responsive_dash_board/widgets/all_expenses_header.dart';
+
+class IncomeHeaderWidget extends StatelessWidget {
+  const IncomeHeaderWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Income',
+          style: Styles.textStyle20.copyWith(color: kTextColor),
+        ),
+        const DateExpensesAndIncome(),
+      ],
+    );
+  }
+}
--- lib/widgets/income_section.dart
+++ lib/widgets/income_section.dart
@@ -2,36 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/core/utilts/constants.dart';
 import 'package:responsive_dash_board/core/utilts/styles.dart';
 import 'package:responsive_dash_board/widgets/all_expenses_header.dart';
+import 'package:responsive_dash_board/widgets/income_header_widget.dart';
 
 class IncomeSection extends StatelessWidget {
   const IncomeSection({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return   Column(
-      children: [
-           IncomeHeaderWidget()
-      ],
-    );
+    return const   IncomeHeaderWidget();
   }
 }
 
-class IncomeHeaderWidget extends StatelessWidget {
-  const IncomeHeaderWidget({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-     mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-       Text('Income',style: Styles.textStyle20.
-       copyWith(
-           color: kTextColor
-       ),),
-       const  DateExpensesAndIncome(),
-      ],
-    );
-  }
-}
