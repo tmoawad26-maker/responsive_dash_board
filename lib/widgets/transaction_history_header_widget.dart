3393075fc51a96c6b6431d3edef081aeff218a4d
sh2 <tmoawad26@gamil.com>
2026-05-07 16:10:36 +0300

    creating transaction history header widget

---
+++ lib/widgets/transaction_history_header_widget.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/core/utilts/constants.dart';
+import 'package:responsive_dash_board/core/utilts/styles.dart';
+
+class TransactionHistoryHeaderWidget extends StatelessWidget {
+  const TransactionHistoryHeaderWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          'Transaction History',
+          style: Styles.textStyle20.copyWith(color: kTextColor),
+        ),
+        Text(
+          'See all',
+          style: Styles.textStyle16
+              .copyWith(color: kPrimaryColor, fontWeight: FontWeight.w500),
+        )
+      ],
+    );
+  }
+}
--- lib/widgets/transaction_history_widget.dart
+++ lib/widgets/transaction_history_widget.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dash_board/core/utilts/constants.dart';
 import 'package:responsive_dash_board/core/utilts/styles.dart';
-import 'package:responsive_dash_board/widgets/transaction_history_item.dart';
+import 'package:responsive_dash_board/widgets/transaction_history_header_widget.dart';
 import 'package:responsive_dash_board/widgets/transaction_history_list_view.dart';
 
 class TransactionHistoryWidget extends StatelessWidget {
@@ -10,45 +9,22 @@ class TransactionHistoryWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-       const  TransactionHistoryHeaderWidget(),
-       const  SizedBox(height: 16,),
-       Text('13 April 2022',style: Styles.textStyle16.copyWith(
-        fontWeight: FontWeight.w500,
-        color: const  Color(0xffAAAAAA)
-       ),),
-
-       const   SizedBox(height: 56,),
-       const   TransactionHistoryListView()
-      ],
-    );
-  }
-}
-
-class TransactionHistoryHeaderWidget extends StatelessWidget {
-  const TransactionHistoryHeaderWidget({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Text(
-          'Transaction History',
-          style: Styles.textStyle20.copyWith(color: kTextColor),
+        const TransactionHistoryHeaderWidget(),
+        const SizedBox(
+          height: 20,
         ),
         Text(
-          'See all',
-          style: Styles.textStyle16
-              .copyWith(color: kPrimaryColor, fontWeight: FontWeight.w500),
-        )
+          '13 April 2022',
+          style: Styles.textStyle16.copyWith(
+              fontWeight: FontWeight.w500, color: const Color(0xffAAAAAA)),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        const TransactionHistoryListView()
       ],
     );
   }
 }
-
-
-
